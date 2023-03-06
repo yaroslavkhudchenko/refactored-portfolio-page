bf0dafb5430d8920c3a6a8bc8dd8422b9c17de00
Yari PL <yaroslav.khudchenko@gmail.com>
2023-03-06 09:38:29 +0100

    temporary portfolio change

--- src/Components/Portfolio/portfolioData.ts
+++ src/Components/Portfolio/portfolioData.ts
@@ -11,7 +11,6 @@ type Projects = {
   linkUrl: string
 }
 
-
 export const PROJECTS_DATA: Projects[] = [
     {
       title: 'T-REX ENDLESS RUNNER',
@@ -20,22 +19,23 @@ export const PROJECTS_DATA: Projects[] = [
       descr:
         '3D clone of google chrome browser t-rex game made in 3D with the help of Three.js framework',
       linkCode: 'https://github.com/YariPL/404-trex-clone',
-      linkUrl: 'https://dino3d.herokuapp.com/',
-    },
-    {
-      title: 'Task manager',
-      techStack: ['react', 'mongodb', 'nodejs', 'express', 'rest', 'sass'],
-      videoUrl: taskVideo,
-      descr: 'Task manager is the app which helps to manage projects and tasks',
-      linkCode: 'https://github.com/YariPL/task-manager',
-      linkUrl: 'https://tasksorginizer.herokuapp.com/',
-    },
-    {
-      title: 'Pathfinding visualizer',
-      techStack: ['react', 'sass'],
-      videoUrl: pathVideo,
-      descr: 'Visualization of how the pathfinding algorithm works',
-      linkCode: 'https://github.com/YariPL/pathfinder-algorithms-visualizer',
-      linkUrl: 'https://pathfindingvisualizeralg.herokuapp.com/',
-    },
+      linkUrl: 'https://trex-clone.khudchenko.dev/',
+    }
+    // ,
+    // {
+    //   title: 'Task manager',
+    //   techStack: ['react', 'mongodb', 'nodejs', 'express', 'rest', 'sass'],
+    //   videoUrl: taskVideo,
+    //   descr: 'Task manager is the app which helps to manage projects and tasks',
+    //   linkCode: 'https://github.com/YariPL/task-manager',
+    //   linkUrl: 'https://tasksorginizer.herokuapp.com/',
+    // },
+    // {
+    //   title: 'Pathfinding visualizer',
+    //   techStack: ['react', 'sass'],
+    //   videoUrl: pathVideo,
+    //   descr: 'Visualization of how the pathfinding algorithm works',
+    //   linkCode: 'https://github.com/YariPL/pathfinder-algorithms-visualizer',
+    //   linkUrl: 'https://pathfindingvisualizeralg.herokuapp.com/',
+    // },
   ]
